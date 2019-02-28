9528e4efab4e01ecf435350a8664eb9f8f636f3d
Jose Zavala <jmzg1229@gmail.com>
2019-02-28 14:58:59 -0500

    Fixed Trim State Calculations
    - Lowered minimum thrust value for trim state calculation. Min was 0.1, solution was 0.0778
    - Removed real-time pacing while not testing with pilots

--- calculate_trimmed_state.m
+++ calculate_trimmed_state.m
@@ -10,7 +10,7 @@ norm_body_force = calculate_body_force(inputs0,airplane);
 
 myfunc = @(inputs) calculate_body_force(inputs,airplane);
 
-min_inputs = [-10, -10, 0.1];
+min_inputs = [-10, -10, 0.01];
 max_inputs = [40, 30, 0.5];
 
 trimmed_inputs = fmincon(myfunc,inputs0,[],[],[],[],min_inputs,max_inputs);
